2f1f9234f9145259afbe9213fbc2d33073313b8c
ivarFastackai <rbhushan1122@gmail.com>
2026-07-20 23:25:53 +0530

    Rust basics: conditionals, loops, variables, functions

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    
+ 
+    // let res = add(10, 20);
+    // print!("{}",res);
+    // let age = 15;
+    // if age<13{
+    //     print!("You are teenager");
+    // }else if age<18 && age<13 {
+    //     print!("you are kid");
+    // }else {
+    //     print!("adult");
+    // }
+
+
+    // loops
+
+              
+}
+
+
+// fn add(a:i32,b:i32)->i32{
+//     return a+b;
+// }
